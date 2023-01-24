d7d8eac0b758c8b76d05a96223ffea03676aa821
dickyganda <57479061+dickyganda@users.noreply.github.com>
2023-01-24 21:14:36 +0700

    create report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,19 +6,40 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Session;
 
-use App\Models\T_Kas;
+use App\Models\T_Penjualan;
+use App\Models\DT_Penjualan;
 
 class ReportController extends Controller
 {
 
     function Index()
     {
+        $report_all = DB::table('t_penjualan')
+            ->join('dt_penjualan', 'dt_penjualan.id_t_penjualan', '=', 't_penjualan.id_penjualan')
+            ->join('m_barang', 'm_barang.id_barang', '=', 'dt_penjualan.id_barang')
+            ->join('m_rekanan', 'm_rekanan.id_rekanan', '=', 'm_barang.id_rekanan')
+            ->get();
+
+        $grand_total = DT_Penjualan::selectRaw('sum(total_penjualan) as total')
+            ->whereDate('tgl_transaksi_penjualan', Date('Y-m-d'))
+            ->first()->total;
+        // dd($grand_total);
+
+        // $grand_total = DB::table('t_penjualan')
+        //     ->join('dt_penjualan', 'dt_penjualan.id_t_penjualan', '=', 't_penjualan.id_penjualan')
+        //     ->join('m_barang', 'm_barang.id_barang', '=', 'dt_penjualan.id_barang')
+        //     ->join('m_rekanan', 'm_rekanan.id_rekanan', '=', 'm_barang.id_rekanan')
+        //     ->select('m_rekanan.nama_rekanan', 'dt_penjualan.total_penjualan', DB::raw('SUM(total_penjualan) as total'))
+        //     ->groupBy('m_rekanan.nama_rekanan')
+        //     ->get();
+        // ->selectRaw('sum(dt_penjualan.total_penjualan) as total')->first()->total;
+        // dd($grand_total);
 
         return view(
             'report/index',
             [
-                // 'dataSaldo' => $dataSaldo,
-                // 't_kas' => $t_kas,
+                'report_all' => $report_all,
+                'grand_total' => $grand_total,
                 // 'databarang' => $databarang,
                 // 'datarekanan' => $datarekanan,
 
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -32,7 +32,73 @@ Report
 
                 <div class="card card-primary card-outline">
                     <div class="card-body">
+                        {{-- <a href="" class="btn btn-success btn-xs" title="Tambah Data Baru" role="button" data-toggle="modal" data-target="#modaltambahdata"><i class="fas fa-plus-circle"></i>Tambah</a><br><br> --}}
 
+                        <table border="0" cellspacing="5" cellpadding="5">
+                            <tbody>
+                                <tr>
+                                    <td><input type="text" id="min" name="min" value="<?php echo date('d-m-Y'); ?>">
+                                    </td>
+                                    <td>-</td>
+                                    <td><input type="text" id="max" name="max" value="<?php echo date('d-m-Y'); ?>">
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+
+                        <table id="dt-basic-example" class="table table-bordered table-responsive table-hover table-striped">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Rekanan</th>
+                                    <th>Nama Barang</th>
+                                    <th>Qty Penjualan</th>
+                                    <th>Price</th>
+                                    <th>Tgl_penjualan</th>
+                                    <th>Grand Total</th>
+
+                                </tr>
+                            </thead>
+                            <tbody height="10px">
+                                @php $i=1 @endphp
+                                @foreach($report_all as $report)
+                                <tr>
+                                    <td>{{ $i++ }}</td>
+                                    <td>{{ $report->nama_rekanan }}</td>
+                                    <td>{{ $report->nama_barang }}</td>
+                                    <td>{{ $report->qty_penjualan }}</td>
+                                    <td>{{ $report->harga_barang }}</td>
+                                    <td>{{ $report->tgl_transaksi_penjualan }}</td>
+                                    <td>{{ $report->total_penjualan }}</td>
+                                    {{-- <td>
+
+                                        <a href="/transaksicuci/edittransaksicuci/{{ $cuci->id_cuci }}" title="Edit" class="btn btn-warning btn-xs" role="button"><i class="fas fa-pen"></i></a>
+                                    <a href="#" title="Edit" class="btn btn-warning btn-xs" role="button"><i class="fas fa-print"></i></a>
+
+                                    <a href="#" onclick="deleteharga({{$harga->id_harga}})" title="Hapus" class="btn btn-danger btn-xs" role="button"><i class="fas fa-trash"></i></a>
+
+                                    </td> --}}
+                                </tr>
+                                @endforeach
+
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Rekanan</th>
+                                    <th>Nama Barang</th>
+                                    <th>Qty Penjualan</th>
+                                    <th>Price</th>
+                                    <th>Tgl_penjualan</th>
+                                    <th>Grand Total</th>
+                                </tr>
+                                <tr>
+                                    <td colspan="6"> Grand Total</td>
+                                    <td>{{$grand_total}}</td>
+                                </tr>
+                            </tfoot>
+                        </table>
 
                     </div><!-- /.card -->
                 </div>
@@ -74,11 +140,36 @@ Report
                 format: 'DD-MM-YYYY'
             });
             var table = $('#dt-basic-example').DataTable({
-                initComplete: function() {
+                dom: 'Bfrtip'
+                , buttons: [
+                    'excel', 
+                    , {
+                        extend: 'pdfHtml5'
+                        , orientation: 'landscape'
+                        , pageSize: 'A4'
+                    }
+                , ]
+                , initComplete: function() {
                     this.api()
                         .columns()
                         .every(function() {
-
+                            var column = this;
+                            var select = $('<select><option value=""></option></select>')
+                                .appendTo($(column.footer()).empty())
+                                .on('change', function() {
+                                    var val = $.fn.dataTable.util.escapeRegex($(this)
+                                        .val());
+                                    column.search(val ? '^' + val + '$' : '', true
+                                        , false).draw();
+                                });
+                            column
+                                .data()
+                                .unique()
+                                .sort()
+                                .each(function(d, j) {
+                                    select.append('<option value="' + d + '">' + d +
+                                        '</option>');
+                                });
                         });
                 }
             , });
@@ -86,7 +177,6 @@ Report
             $('#min, #max').on('change', function() {
                 table.draw();
             });
-
         });
 
         $("#tambahkas").submit(function(event) {
